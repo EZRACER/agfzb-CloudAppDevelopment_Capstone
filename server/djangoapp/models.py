18c627573de5e3c5d93b825d2fc7441f98489dcd
EZRACER <yusufseward@gmail.com>
2022-12-18 00:06:52 -0500

    Updated files to finish week 3

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,26 +2,84 @@ from django.db import models
 from django.utils.timezone import now
 
 
-# Create your models here.
+class CarMake(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(null=False, max_length=50)
+    description = models.CharField(max_length=1000)
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
+
+class CarModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField(null=False)
+    name = models.CharField(null=False, max_length=50)
+    year = models.DateField()
+
+    SUV = 'SUV'
+    SEDAN = 'Sedan'
+    WAGON = 'Wagon'
+
+    TYPES = [
+        (SUV, 'SUV'),
+        (SEDAN, 'Sedan'),
+        (WAGON, 'Wagon'),
+    ]
+
+    type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPES,
+        default=SEDAN
+    )
+
+    def __str__(self):
+        return self.make.name + " " + \
+               self.name + ", " + \
+               str(self.year.year) + "(" + \
+               "DealerID: " + str(self.dealer_id) + ")"
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class DealerReview:
+    def __init__(self, dealer_id, name, purchase, review, id=0, make=None, model=None, year=None, purchase_date=None):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.dealer_id = dealer_id
+        self.id = id
+        self.name = name
+        self.purchase = purchase
+        self.purchase_date = purchase_date
+        self.review = review
+        self.sentiment = ""
 
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+    def __str__(self):
+        return "Customer Name: " + self.name
